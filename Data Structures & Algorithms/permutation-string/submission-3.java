37af826393613f2d92322c498f2786698c823983
shrikantsawant399 <shrikantsawant399@gmail.com>
2026-07-25 12:52:52 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s2.length() < s1.length()) return false;
+        int left = 0;
+        int window = s1.length();
+        int right = left + window - 1;
+        int[] s1Arr = new int[26];
+        int[] s2Arr = new int[26]; 
+
+        for(int i = 0; i < window; i++){
+            s1Arr[s1.charAt(i) - 'a'] = s1Arr[s1.charAt(i) - 'a'] + 1;
+            s2Arr[s2.charAt(i) - 'a'] = s2Arr[s2.charAt(i) - 'a'] + 1;
+        }
+        if(Arrays.equals(s1Arr, s2Arr)){
+            return true;
+        }
+        left++;
+        right++;
+
+        while(right < s2.length()){
+            s2Arr[s2.charAt(right) - 'a'] = s2Arr[s2.charAt(right) - 'a'] + 1;
+            s2Arr[s2.charAt(left - 1) - 'a'] = s2Arr[s2.charAt(left - 1) - 'a'] - 1;
+            if(Arrays.equals(s1Arr, s2Arr)){
+                return true;
+            }
+            left++;
+            right++;
+        }
+        return false;
+    }
+}
