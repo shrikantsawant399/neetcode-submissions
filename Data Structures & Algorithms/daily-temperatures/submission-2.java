e9ce3b072f977c758cd25d27e544742557b8a304
shrikantsawant399 <shrikantsawant399@gmail.com>
2026-07-25 20:56:05 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        int index = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(index);
+        index++;
+
+        while(index < temperatures.length){
+            if(stack.isEmpty()){
+                stack.push(index);
+                continue;
+            }
+            if(temperatures[index] > temperatures[stack.peek()]){
+                result[stack.peek()] = index - stack.peek();
+                stack.pop();
+            }else{
+                stack.push(index);
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
